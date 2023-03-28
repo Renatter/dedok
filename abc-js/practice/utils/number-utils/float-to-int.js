3e663c77029b76b6ae36d2551d1318e6f3b5ce9e
Renatter <riko321riko321@gmail.com>
2023-03-28 15:38:00 +0500

    pad.js FloatToInt

--- abc-js/practice/utils/number-utils/float-to-int.js
+++ abc-js/practice/utils/number-utils/float-to-int.js
@@ -1,3 +1,8 @@
+import { checkNumberForError } from "../string-utils/common.js";
+
 /** возвращает целую часть дробного числа. -56.78 -> -56*/
 export function floatToInt(value) {
+  checkNumberForError(value);
+  let x = value % 1;
+  return value - x;
 }
--- abc-js/practice/utils/string-utils/pad.js
+++ abc-js/practice/utils/string-utils/pad.js
@@ -1,17 +1,66 @@
+import { repeat } from "./repeat.js";
+import { slice } from "./slice.js";
+import { floatToInt } from "../number-utils/float-to-int.js";
+import { checkIsString } from "./common.js";
+import { len } from "./len.js";
+
 /** Возвращает копию text увеличенный до длины maxLength
  * заполненный в начале символами fillString.
  * Допускается в fillString передавать строку из нескольких символов. */
-export function padStart(text, maxLength, fillString = ' ') {
+export function padStart(text, maxLength, fillString = " ") {
+  checkIsString(text, " text");
+  checkIsString(fillString, " fillString");
+  let textLength = len(text);
+  let fillStringLength = len(fillString);
+
+  if (typeof maxLength === "undefined" || maxLength === null) {
+    return text;
+  }
+  if (typeof maxLength !== "number") {
+    throw new Error("invalid type of maxLength");
+  }
+  if (maxLength < textLength) {
+    return text;
+  }
+
+  maxLength = maxLength - textLength;
+  let parseRepeat = maxLength / fillStringLength;
+
+  if (maxLength > fillStringLength) {
+    fillString += repeat(fillString, floatToInt(parseRepeat));
+  }
+  return slice(fillString, 0, maxLength) + text;
 }
 
 /** Возвращает копию text увеличенный до длины maxLength
  * заполненный в конце символами fillString.
  * Допускается в fillString передавать строку из нескольких символов. */
-export function padEnd(text, maxLength, fillString = ' ') {
+export function padEnd(text, maxLength, fillString = " ") {
+  checkIsString(text, " text");
+  checkIsString(fillString, " fillString");
+  let textLength = len(text);
+  let fillStringLength = len(fillString);
+
+  if (typeof maxLength === "undefined" || maxLength === null) {
+    return text;
+  }
+  if (typeof maxLength !== "number") {
+    throw new Error("invalid type of maxLength");
+  }
+  if (maxLength < textLength) {
+    return text;
+  }
+
+  maxLength = maxLength - text.length; //3
+  let pareseRepeat = maxLength / fillString.length; //3
+
+  if (maxLength > fillString.length) {
+    fillString += repeat(fillString, floatToInt(pareseRepeat));
+  }
+  return text + slice(fillString, 0, maxLength);
 }
 
 /** Возвращает копию text увеличенный до длины maxLength
  * заполненный в начале и конце символами fillString.
  * Допускается в fillString передавать строку из нескольких символов. */
-export function pad(text, maxLength, fillString = ' ') {
-}
+export function pad(text, maxLength, fillString = " ") {}
